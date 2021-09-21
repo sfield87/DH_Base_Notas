1fec581ab84aba3186536c0e28749a69a9028a8a
sfield87 <sfield87@gmail.com>
2021-09-21 10:23:40 -0300

    Mercado Liebre

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -4,21 +4,11 @@ const path = require('path');
 const pathPublic = path.resolve(__dirname, 'public');
 
 app.use(express.static(pathPublic));
-app.listen(3030);
+app.listen(process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/views/home.html')),
     console.log("Servidor Levantado en puerto 3030")
-});
-
-app.get('/home_alter', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/home_mio.html'))
-
-});
-
-app.get('/registrar', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/registrar.html'))
-
 });
 
 app.get('/login', (req, res) => {
@@ -26,10 +16,6 @@ app.get('/login', (req, res) => {
 
 });
 
-app.get('/contacto', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/contacto.html'))
-
-});
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/views/register.html'))
 
